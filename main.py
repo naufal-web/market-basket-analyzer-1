2d1e7c86b380810fb1604f01fa562cb5a4138dcd
Naufal Maulana Malik <naufalmaulanamaliksr@gmail.com>
2023-08-29 07:21:51 +0700

    Add features highlight

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ try:
 except ModuleNotFoundError:
     print("Kedua modul belum ada")
 
+# tampilkan judul program
 st.title("Market Basket Analyzer - Modified 1")
+# tampilkan subheader
 st.subheader("Powered by Naufal-Web")
 
+# tampilkan fitur unggah file
 with st.expander("Unggah file CSV"):
     csv_file_readable = st.file_uploader("Unggah file CSV", type="csv")
+
